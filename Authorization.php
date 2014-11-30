5ff27d3dbf13c130c609136aef4161ee4cd14d01
crynobone <crynobone@gmail.com>
2014-11-30 14:45:52 +0800

    Move authorization to it's own namespace.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ Authorization.php
@@ -0,0 +1,242 @@
+<?php namespace Orchestra\Auth\Authorization;
+
+use RuntimeException;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use Orchestra\Contracts\Auth\Guard;
+use Orchestra\Memory\ContainerTrait;
+use Orchestra\Contracts\Memory\Provider;
+use Orchestra\Contracts\Authorization\Authorization as AuthorizationContract;
+
+class Authorization implements AuthorizationContract
+{
+    use AuthorizationTrait, ContainerTrait;
+
+    /**
+     * Acl instance name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Construct a new object.
+     *
+     * @param  \Orchestra\Contracts\Auth\Guard  $auth
+     * @param  string  $name
+     * @param  \Orchestra\Contracts\Memory\Provider|null  $memory
+     */
+    public function __construct(Guard $auth, $name, Provider $memory = null)
+    {
+        $this->auth    = $auth;
+        $this->name    = $name;
+        $this->roles   = new Fluent('roles');
+        $this->actions = new Fluent('actions');
+
+        $this->roles->add('guest');
+        $this->attach($memory);
+    }
+
+    /**
+     * Bind current ACL instance with a Memory instance.
+     *
+     * @param  \Orchestra\Contracts\Memory\Provider  $memory
+     * @return void
+     * @throws \RuntimeException if $memory has been attached.
+     */
+    public function attach(Provider $memory = null)
+    {
+        if ($this->attached() && $memory !== $this->memory) {
+            throw new RuntimeException(
+                "Unable to assign multiple Orchestra\Memory instance."
+            );
+        }
+
+        // since we already check instanceof Orchestra\Memory\Provider,
+        // It safe to just check for not NULL.
+        if (! is_null($memory)) {
+            $this->setMemoryProvider($memory);
+            $this->initiate();
+        }
+    }
+
+    /**
+     * Initiate acl data from memory.
+     *
+     * @return $this
+     */
+    protected function initiate()
+    {
+        $name = $this->name;
+        $data = array('acl' => array(), 'actions' => array(), 'roles' => array());
+        $data = array_merge($data, $this->memory->get("acl_{$name}", array()));
+
+        // Loop through all the roles and actions in memory and add it to
+        // this ACL instance.
+        $this->roles->attach($data['roles']);
+        $this->actions->attach($data['actions']);
+
+        // Loop through all the acl in memory and add it to this ACL
+        // instance.
+        foreach ($data['acl'] as $id => $allow) {
+            list($role, $action) = explode(':', $id);
+            $this->assign($role, $action, $allow);
+        }
+
+        return $this->sync();
+    }
+
+    /**
+     * Assign single or multiple $roles + $actions to have access.
+     *
+     * @param  string|array  $roles      A string or an array of roles
+     * @param  string|array  $actions    A string or an array of action name
+     * @param  bool          $allow
+     * @return $this
+     */
+    public function allow($roles, $actions, $allow = true)
+    {
+        $this->setAuthorization($roles, $actions, $allow);
+
+        return $this->sync();
+    }
+
+    /**
+     * Verify whether current user has sufficient roles to access the
+     * actions based on available type of access.
+     *
+     * @param  string  $action     A string of action name
+     * @return bool
+     */
+    public function can($action)
+    {
+        $roles = $this->getUserRoles();
+
+        return $this->checkAuthorization($roles, $action);
+    }
+
+    /**
+     * Verify whether given roles has sufficient roles to access the
+     * actions based on available type of access.
+     *
+     * @param  string|array  $roles      A string or an array of roles
+     * @param  string        $action     A string of action name
+     * @return bool
+     * @throws \InvalidArgumentException
+     */
+    public function check($roles, $action)
+    {
+        return $this->checkAuthorization($roles, $action);
+    }
+
+    /**
+     * Shorthand function to deny access for single or multiple
+     * $roles and $actions.
+     *
+     * @param  string|array  $roles      A string or an array of roles
+     * @param  string|array  $actions    A string or an array of action name
+     * @return $this
+     */
+    public function deny($roles, $actions)
+    {
+        return $this->allow($roles, $actions, false);
+    }
+
+    /**
+     * Sync memory with acl instance, make sure anything that added before
+     * ->with($memory) got called is appended to memory as well.
+     *
+     * @return $this
+     */
+    public function sync()
+    {
+        if ($this->attached()) {
+            $name = $this->name;
+
+            $this->memory->put("acl_{$name}", array(
+                "acl"     => $this->acl,
+                "actions" => $this->actions->get(),
+                "roles"   => $this->roles->get(),
+            ));
+        }
+
+        return $this;
+    }
+
+    /**
+     * Forward call to roles or actions.
+     *
+     * @param  string  $type           'roles' or 'actions'
+     * @param  string  $operation
+     * @param  array   $parameters
+     * @return \Orchestra\Auth\Authorization\Fluent
+     */
+    public function execute($type, $operation, array $parameters = array())
+    {
+        return call_user_func_array(array($this->{$type}, $operation), $parameters);
+    }
+
+    /**
+     * Magic method to mimic roles and actions manipulation.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, array $parameters)
+    {
+        list($type, $operation) = $this->resolveDynamicExecution($method);
+
+        $response = $this->execute($type, $operation, $parameters);
+
+        if ($operation === 'has') {
+            return $response;
+        }
+
+        return $this->sync();
+    }
+
+    /**
+     * Dynamically resolve operation name especially to resolve attach and
+     * detach multiple actions or roles.
+     *
+     * @param  string  $method
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    protected function resolveDynamicExecution($method)
+    {
+        // Preserve legacy CRUD structure for actions and roles.
+        $method  = Str::snake($method, '_');
+        $matcher = '/^(add|rename|has|get|remove|fill|attach|detach)_(role|action)(s?)$/';
+
+        if (! preg_match($matcher, $method, $matches)) {
+            throw new InvalidArgumentException("Invalid keyword [$method]");
+        }
+
+        $type      = $matches[2].'s';
+        $multiple  = (isset($matches[3]) && $matches[3] === 's');
+        $operation = $this->resolveOperationName($matches[1], $multiple);
+
+        return array($type, $operation);
+    }
+
+    /**
+     * Dynamically resolve operation name especially when multiple
+     * operation was used.
+     *
+     * @param  string  $operation
+     * @param  bool    $multiple
+     * @return string
+     */
+    protected function resolveOperationName($operation, $multiple = true)
+    {
+        if (! $multiple) {
+            return $operation;
+        } elseif (in_array($operation, array('fill', 'add'))) {
+            return 'attach';
+        }
+
+        return 'detach';
+    }
+}
---
+++ Factory.php
@@ -0,0 +1,130 @@
+<?php namespace Orchestra\Auth\Authorization;
+
+use Illuminate\Support\Arr;
+use Orchestra\Contracts\Auth\Guard;
+use Orchestra\Contracts\Memory\Provider;
+use Orchestra\Contracts\Authorization\Factory as FactoryContract;
+
+class Factory implements FactoryContract
+{
+    /**
+     * Auth instance.
+     *
+     * @var \Orchestra\Contracts\Auth\Guard
+     */
+    protected $auth;
+
+    /**
+     * Cache ACL instance so we can reuse it on multiple request.
+     *
+     * @var array
+     */
+    protected $drivers = [];
+
+    /**
+     * Construct a new Environment.
+     *
+     * @param  \Orchestra\Contracts\Auth\Guard  $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Initiate a new ACL Container instance.
+     *
+     * @param  string  $name
+     * @param  \Orchestra\Contracts\Memory\Provider  $memory
+     * @return \Orchestra\Contracts\Authorization\Authorization
+     */
+    public function make($name = null, Provider $memory = null)
+    {
+        if (is_null($name)) {
+            $name = 'default';
+        }
+
+        if (! isset($this->drivers[$name])) {
+            $this->drivers[$name] = new Authorization($this->auth, $name, $memory);
+        }
+
+        return $this->drivers[$name];
+    }
+
+    /**
+     * Register an ACL Container instance with Closure.
+     *
+     * @param  string  $name
+     * @param  \Closure  $callback
+     * @return \Orchestra\Contracts\Authorization\Authorization
+     */
+    public function register($name, $callback = null)
+    {
+        if (is_callable($name)) {
+            $callback = $name;
+            $name     = null;
+        }
+
+        $instance = $this->make($name);
+
+        call_user_func($callback, $instance);
+
+        return $instance;
+    }
+
+    /**
+     * Manipulate and synchronize roles.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, array $parameters)
+    {
+        $response = array();
+
+        foreach ($this->drivers as $acl) {
+            $response[] = call_user_func_array([$acl, $method], $parameters);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Shutdown/finish all ACL.
+     *
+     * @return $this
+     */
+    public function finish()
+    {
+        // Re-sync before shutting down.
+        foreach ($this->drivers as $acl) {
+            $acl->sync();
+        }
+
+        $this->drivers = [];
+
+        return $this;
+    }
+
+    /**
+     * Get all ACL instances.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $this->drivers;
+    }
+
+    /**
+     * Get ACL instance by name.
+     *
+     * @param  string  $name
+     * @return \Orchestra\Contracts\Authorization\Authorization
+     */
+    public function get($name)
+    {
+        return Arr::get($this->drivers, $name);
+    }
+}
